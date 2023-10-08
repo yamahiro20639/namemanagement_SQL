0cf44e3d70534b5f156267465ebe533a9946ae83
yamahiro20639 <144509349+yamahiro20639@users.noreply.github.com>
2023-10-08 22:21:42 +0900

    プロジェクト作成

---
+++ src/main/java/com/yamasaki/namemanagement/NamemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.yamasaki.namemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamemanagementApplication.class, args);
+	}
+
+}
