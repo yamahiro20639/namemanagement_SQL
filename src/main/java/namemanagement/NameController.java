20822378e60b56ffc796eca5c39499e5b1a3a50f
yamahiro20639 <144509349+yamahiro20639@users.noreply.github.com>
2023-10-09 10:02:57 +0900

    Controllerを実装

--- src/main/java/namemanagement/NameController.java
+++ src/main/java/namemanagement/NameController.java
@@ -1,2 +1,22 @@
-package namemanagement;public class NameController {
+package namemanagement;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class NameController {
+
+    private NameService nameService;
+
+    public NameController(NameService nameService) {
+        this.nameService = nameService;
+    }
+
+    @GetMapping("/names")
+    public List<Name> getNames() {
+        List<Name> names = nameService.getNames();
+        return names;
+    }
 }
